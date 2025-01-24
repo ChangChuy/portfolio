3c8e65d5a521b33bc031b4a9ad20c469bc258d9b
Chang Chuy <changchuy@changs-mbp.home>
2025-01-24 02:05:45 -0500

    Create the exoskeleton for the frontend web page. - will start working on nav bar next.

---
+++ monorepo/frontend/src/app/components/header.tsx
@@ -0,0 +1,15 @@
+
+const Header = () => {
+  return (
+    <header className="bg-white">
+        <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
+      <div>
+      <span className="sr-only">Your Company</span>
+      </div>
+      </nav>
+    </header>
+
+  );
+}
+
+export default Header;
