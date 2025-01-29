e2b305c86bbd841d40d34b0610edf6ea6faa01bc
Chang Chuy <changchuy@changs-mbp.home>
2025-01-28 23:23:17 -0500

    Modified Nav bar link to have hover animation, and current page would be highlighted on the Nav bar as well

--- monorepo/frontend/src/app/components/nav.tsx
+++ monorepo/frontend/src/app/components/nav.tsx
@@ -1,8 +1,58 @@
+"use client"
+
+import Link from "next/link";
+import { usePathname } from "next/navigation"
+
+
+const links = [ 
+    {
+        name: "home",
+        path: "/",
+    },
+    {
+        name: "services",
+        path: "/services",
+    },
+    {
+        name: "resume",
+        path: "/resume",
+    },
+    {
+        name: "projects",
+        path: "/projects",
+    },
+    {
+        name: "contact",
+        path: "/contact",
+    },
+]
+
+
+// highlight the nav button when the use is currently at the path/url
 
 const Nav = () => {
+
+    const pathname = usePathname();
+    console.log(pathname);
     return (
-        <nav> Navigation bar.</nav>
-        );
+        <nav className="flex gap-8">
+            {links.map((link, index) => {
+            const isActive = link.path === pathname;
+
+            return (
+            <Link
+                href={link.path}
+                key={index}
+                className={`capitalize font-medium transition-all duration-300 hover:text-lightGreen border-b-2 ${
+                isActive ? "text-lightGreen border-lightGreen" : "border-transparent hover:border-lightGreen"
+                }`}
+            >
+                {link.name}
+            </Link>
+            );
+        })}
+        </nav>
+    );
 }
 
 export default Nav; 
