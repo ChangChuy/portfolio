1b604f14008becf632a31a14e8604e23a606d7a4
Chang Chuy <changchuy@changs-mbp.home>
2025-02-19 04:03:37 -0500

    homePage done.

--- monorepo/frontend/src/app/components/Photo.tsx
+++ monorepo/frontend/src/app/components/Photo.tsx
@@ -7,10 +7,14 @@ import Image from "next/image";
 const Photo = () => {
   return (
     <div className="w-full h-full relative">
-        <motion.div>
-            <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+        <motion.div initial={{ opacity: 0}} animate={{ opacity: 1, transition: { delay:2, duration: 0.4, ease: "easeIn"}}}>
+            <motion.div initial={{ opacity: 0}} animate={{ opacity: 1, transition: { delay: 2.4, duration: 0.4, ease: "easeInOut"}}} className="-translate-y-3.5 w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute">
                 <Image src="/picture.png" priority quality={100} fill alt="" className="object-contain"/>
-            </div>
+            </motion.div>
+
+            <motion.svg className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]" fill="transparent" viewBox="0 0 506 506" >
+              <motion.circle cx="253" cy="253" r="250" stroke="#00ff99" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" initial={{ strokeDasharray: "24 10 0 0" }} animate={{ strokeDasharray: ["15 120 25 25", "16 25 92 72", "4 250 22 22"], rotate: [120, 360]}} transition={{ duration: 15, repeat: Infinity, repeatType: "reverse"}}/>
+            </motion.svg>
         </motion.div>
     </div>
   )
--- monorepo/frontend/src/app/page.tsx
+++ monorepo/frontend/src/app/page.tsx
@@ -1,39 +1,46 @@
 import { FiDownload } from "react-icons/fi";
 import SocialButtons from "./components/SocialButtons";
 import Photo from "./components/Photo";
+import Stats from "./components/Stats";
+
 
 
 
 export default function Home() {
   return (
-    <div>
-      <div className="container mx-auto h-full">
-        <div className="flex flex-col xl:flex-row items center justify-between xl:pt-8 xl:pb-24">
-          <div className="text-center xl:text-left">
-            <span className="text-xl text-[20px]">
-              Software Developer
-            </span>
-            <h1 className="h1 mb-6"> Hello I'm <br/> <span className="text-lightGreen">Chang Chuy </span> 
+    <div className="min-h-screen flex items-center">  {/* Outer container fills viewport & centers content */}
+      <div className="container mx-auto">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl text-[25px]">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />
+              <span className="text-lightGreen">Chang Chuy</span>
             </h1>
             <p className="max-w-[500px] text-[24px] mb-9 text-white/80 mx-auto">
-              I'm a software engineer, i have Accumulated 4+ years of experience building different side gaming projects with Godot, Web App with JavaScript, React, SQL/NoSQL/GraphQL. Over 4 years of programming experience with a strong drive to work under time constraints and collaborate effectively with teams. 
+              I'm a software engineer, I have accumulated 4+ years of experience building various side gaming projects with Godot, web apps with JavaScript, React, SQL/NoSQL/GraphQL. Over 4 years of programming experience with a strong drive to work under time constraints and collaborate effectively with teams.
             </p>
-            {/* button for socials */}
+            {/* Buttons for socials */}
             <div className="flex flex-col xl:flex-row items-center gap-6">
-            <button className="py-5 px-6 flex items-center text-xl text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
-                Download CV 
-                <FiDownload className="w-5 h-5 ml-2"/>
+              <button className="py-5 px-6 flex items-center text-xl text-blue-700 hover:text-white border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 me-2 mb-2 dark:border-blue-500 dark:text-blue-500 dark:hover:text-white dark:hover:bg-blue-500 dark:focus:ring-blue-800">
+                Download CV
+                <FiDownload className="w-5 h-5 ml-2" />
               </button>
-              <div className="mb-8 xl:mb-8 mt-6">
-                <SocialButtons containerStyles="flex gap-6" iconStyles=" w-9 h-9 border border-blue-700 rounded-full flex justify-center items-center text-blue-700 text-base hover:bg-blue-700 hover:border-blue-700 hover:text-primary hover:transition-all duration-500 "/>
+              <div className="mb-8 xl:mb-0">
+                <SocialButtons
+                  containerStyles="flex gap-6"
+                  iconStyles="w-9 h-9 border border-blue-700 rounded-full flex justify-center items-center text-blue-700 text-base hover:bg-blue-700 hover:border-blue-700 hover:text-primary hover:transition-all duration-500"
+                />
               </div>
             </div>
           </div>
-          <div>
-            <Photo/>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo />
           </div>
         </div>
+        <Stats />
       </div>
+
     </div>
   );
 }
